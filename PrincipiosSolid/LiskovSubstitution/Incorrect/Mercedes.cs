f52a499f152408bc46a97e0d750f0ef2de9fde56
arturoajsi <arturo.a.s.i@hotmail.com>
2021-12-22 16:19:52 +0100

    Add Liskov Substitution incorrect example
    Reference: #6

---
+++ PrincipiosSolid/LiskovSubstitution/Incorrect/Mercedes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.LiskovSubstitution.Incorrect
+{
+    public class Mercedes : Coche
+    {
+        public override int numAsientosRenault()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PrincipiosSolid/LiskovSubstitution/Incorrect/Principal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.LiskovSubstitution.Incorrect
+{
+    public class Principal
+    {
+        static void Main(string[] args)
+        {
+
+            Coche[] arrayCoches = {
+            new Renault(),
+            new Audi(),
+            new Mercedes()
+        };
+            imprimirNumAsientos(arrayCoches);
+        }
+
+        public static void imprimirNumAsientos(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                if (coche.GetType() == typeof(Renault))
+                Console.WriteLine("num asiento 4");
+                if (coche.GetType() == typeof(Audi))
+                    Console.WriteLine("num asiento 5");
+                if (coche.GetType() == typeof(Mercedes))
+                    Console.WriteLine("num asiento 5");
+            }
+        }
+
+    }
+}
