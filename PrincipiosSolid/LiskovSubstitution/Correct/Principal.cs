1d3399620ae83d4b84077a2b8d196e91792f70c9
arturoajsi <arturo.a.s.i@hotmail.com>
2021-12-22 16:31:53 +0100

    Add Liskov Substitution correct example
    closes #5

---
+++ PrincipiosSolid/LiskovSubstitution/Correct/Principal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.LiskovSubstitution.Correct
+{
+    public class Principal
+    {
+        static void Main(string[] args)
+        {
+
+            Coche[] arrayCoches = {
+            new Renault(),
+            new Audi(),
+            new Mercedes()
+        };
+            imprimirNumAsientos(arrayCoches);
+        }
+
+        public static void imprimirNumAsientos(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                Console.WriteLine(coche.numAsientosRenault());
+            }
+        }
+
+    }
+}
